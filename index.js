f405d2cfa000c34c937d346127252f77e561c9e6
Emmi Lindsey <emmilindsey@gmail.com>
2023-11-06 13:18:35 -0700

    sending data with fetch lab - unsure why tests aren't passing

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData() {
+
+fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: "Nugget the Cat",
+        email: "nuggetthecat@gmail.com"
+    }),
+})
+.then(r => r.json())
+.then(data => {
+    let newId = data.id
+    newId = document.body.append(newId)
+} )
+
+.catch((error) => {
+    error = {
+        message: "Something did not work!"
+    }
+
+const errorMessage = document.body.append(error.message)
+})
+
+return fetch()
+
+}
+
+submitData()
+
+
